0f9579a42128f4beaffcc3f0060289bd3723a04a
Jungwoo-20 <jungwoo7250@naver.com>
2021-11-30 17:50:51 +0900

    주문 서비스 Initial commit

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemService itemService;
+
+    public Long Order(Long memberid, Long itemId, int quantity) {
+        Member member = memberRepository.findOne(memberid);
+        Item item = itemService.findOne(itemId);
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), quantity);
+        Order order = Order.createOrder(member, delivery, orderItem);
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+
+    }
+}
