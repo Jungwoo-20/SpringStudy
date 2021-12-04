12b53fa6d1c840995bedb2960701b37078ffe9cd
Jungwoo-20 <jungwoo7250@naver.com>
2021-12-04 17:04:51 +0900

    ArgsConstructor 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,12 +1,15 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 
 @Data
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String city;
     private String street;
