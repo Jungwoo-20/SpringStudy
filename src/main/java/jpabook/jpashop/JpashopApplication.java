0aff1bfdd780f50205cc102f66661c0d314c5624
Jungwoo-20 <jungwoo7250@naver.com>
2021-12-04 17:06:37 +0900

    Initial commit

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.study.springboot.StartingEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpashopApplication.class, args);
-//        SpringApplication application = new SpringApplication(JpashopApplication.class);
-//        application.addListeners(new StartingEvent());
-//        application.run(args);
+        SpringApplication application = new SpringApplication(JpashopApplication.class);
+        application.run(args);
     }
 
 }
