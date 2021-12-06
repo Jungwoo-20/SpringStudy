112d7b239a86acd88a0a503f1988918811e4c836
Jungwoo-20 <jungwoo7250@naver.com>
2021-12-06 13:55:51 +0900

    readOnly 설정 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -4,16 +4,19 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+// 단순히 조회 기능만 수행하는 method는 readOnly = true로 하게 되면 JPA와 데이터베이스 측면에서 성능 향상을 높일 수 있음(더디 체킹 과정 생략, 메모리 효율)
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    // readOnly = False
+    @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
