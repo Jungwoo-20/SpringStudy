df97720205cde24ffd0ccf7d7a7aa25f00266b24
Jungwoo-20 <jungwoo7250@naver.com>
2021-12-11 21:54:14 +0900

    updateItem() 변경감지 코드 변경

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -61,14 +61,17 @@ public class ItemController {
 
     @PostMapping("item/edit")
     public String updateItem(BookForm bookForm) {
-        Book book = new Book();
-        book.setId(bookForm.getId());
-        book.setName(bookForm.getName());
-        book.setPrice(bookForm.getPrice());
-        book.setStockQuantity(bookForm.getStockQuantity());
-        book.setAuthor(bookForm.getAuthor());
-        book.setIsbn(bookForm.getIsbn());
-        itemService.saveItem(book);
+//        Book book = new Book();
+//        book.setId(bookForm.getId());
+//        book.setName(bookForm.getName());
+//        book.setPrice(bookForm.getPrice());
+//        book.setStockQuantity(bookForm.getStockQuantity());
+//        book.setAuthor(bookForm.getAuthor());
+//        book.setIsbn(bookForm.getIsbn());
+//        itemService.saveItem(book);
+
+        // 변경 감지 활용 (이게 효율적이다.)
+        itemService.updateItem(bookForm.getId(), bookForm.getName(), bookForm.getPrice(), bookForm.getStockQuantity());
         return "redirect:/items";
     }
 }
